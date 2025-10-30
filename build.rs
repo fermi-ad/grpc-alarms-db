de2db8361e405351493804913a84fbb9d6a6296d
Jacob Curley <jcurley@fnal.gov>
2025-10-30 08:52:33 -0500

    Use proto def from interface-definitions repo (#3)
    Resolves #2

--- build.rs
+++ build.rs
@@ -1,7 +1,8 @@
 use std::{env, path::PathBuf};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let incl: &[&str] = &["src/proto"];
+    const ALARM_PROTO_DIR: &str = "extern/proto-defs/proto/controls/service/grpc-alarms-db/v1";
+    let incl: &[&str] = &[ALARM_PROTO_DIR];
     let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
     let protoc_path = protoc_bin_vendored::protoc_bin_path()?;
     unsafe { std::env::set_var("PROTOC", protoc_path) };
@@ -11,7 +12,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         .build_server(true)
         .protoc_arg("--experimental_allow_proto3_optional")
         .file_descriptor_set_path(out_dir.join("alarmprotos_descriptor.bin"))
-        .compile_protos(&["src/proto/lists.proto"], incl)?;
+        .compile_protos(&[format!("{}/alarm-lists.proto", ALARM_PROTO_DIR)], incl)?;
 
     Ok(())
 }
